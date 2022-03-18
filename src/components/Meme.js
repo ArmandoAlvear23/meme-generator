f5f6ad862b927357e482d7438850f0830cf36918
Armando Alvear <armando.alvear01@outlook.com>
2022-03-17 21:41:49 -0500

    add image using react state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,25 @@
 import React from "react";
+import memesData from "../data";
 
 export default function Meme() {
+  const [url, setUrl] = React.useState("");
+
+  function getMemeImg() {
+    const memesArray = memesData.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArray.length);
+    setUrl(memesArray[randomNum].url);
+  }
+
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image</button>
+        <button className="form--button" onClick={getMemeImg}>
+          Get a new meme image
+        </button>
       </div>
+      <img src={url} className="meme--image" />
     </main>
   );
 }
